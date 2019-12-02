ebac6c7535b73acc65720661af033961d355da84
Seanhetzel <seanhetzel1@gmail.com>
2019-12-02 14:24:16 -0800

    removed full page scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactFullpage from "@fullpage/react-fullpage";
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/Header";
@@ -8,35 +7,18 @@ import SoftwareProjects from "./components/SoftwareProjects";
 import HardwareProjects from "./components/HardwareProjects";
 import Connect from "./components/Connect";
 
-const App = () => (
-    <ReactFullpage
-        // pluginWrapper={pluginWrapper}
-        licenseKey={"Oa02tbC^w3"}
-        scrollingSpeed={500}
-        navigation={true} // use dots navigatio
-        navigationTooltips={["HOME", "SOFTWARE", "HARDWARE", "CONTACT"]}
-        fitToSection={true}
-        cards={true}
-        render={({ state, fullpageApi }) => {
-            return (
-                <ReactFullpage.Wrapper>
-                    <div className="section">
-                        <Header />
-                        <LandingPage />
-                    </div>
-                    <div className="section">
-                        <SoftwareProjects />
-                    </div>
-                    <div className="section">
-                        <HardwareProjects />
-                    </div>
-                    <div className="section">
-                        <Connect />
-                    </div>
-                </ReactFullpage.Wrapper>
-            );
-        }}
-    />
-);
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <LandingPage />
+        <SoftwareProjects />
+        <HardwareProjects />
+        <Connect />
+      </>
+    );
+  }
+}
 
 export default App;
