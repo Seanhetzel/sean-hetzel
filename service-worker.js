4763431d7d42e54c50b36f3cb8c72a74e69c098e
Seanhetzel <seanhetzel1@gmail.com>
2019-12-17 19:02:32 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc73131043dad2418904e3e0671281b4.js"
+  "/precache-manifest.89008729970520c89002d5cef0509adc.js"
 );
 
 self.addEventListener('message', (event) => {
