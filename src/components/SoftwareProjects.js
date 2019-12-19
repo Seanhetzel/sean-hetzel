5fa45b3ae11b1b694eaa142148c7ecf9bc397ecf
Seanhetzel <seanhetzel1@gmail.com>
2019-12-18 19:07:16 -0800

    switched sunset investing to online

--- src/components/SoftwareProjects.js
+++ src/components/SoftwareProjects.js
@@ -62,9 +62,9 @@ export default function SoftwareProjects() {
         "Real estate investing platform where investors can buy/sell fractions of rental properties akin to buying stocks of companies. The advantages for buying just a fraction of rental properties are increased diversification and accessibility. You'll get a fraction of the rent and appreciation.",
       tech: "React, Bootstrap, Rails, JWT",
       finished: false,
-      online: false,
+      online: true,
       soloProject: true,
-      visitLink: "#",
+      visitLink: "https://sean-hetzel.github.io/sunset-investing/",
       gitHubLinks: [
         {
           name: "Frontend GitHub",
