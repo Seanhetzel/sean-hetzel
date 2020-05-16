cafc787332b78a4bdcbbf739a654a29f76d01ca0
Seanhetzel <seanhetzel1@gmail.com>
2020-05-16 13:33:58 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d23e733f756034b976831920425c51f8.js"
+  "/precache-manifest.bc225c1964bdfa0ff3223cb203df3c87.js"
 );
 
 self.addEventListener('message', (event) => {
